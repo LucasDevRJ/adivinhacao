0c882a8e990792d9a52c73f27af0c594ec2f18af
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2024-11-03 20:57:51 -0300

    Editando mensagem do jogo

---
+++ adivinhacao.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+	//Imprime cabeçalho do jogo
+	printf("—————————————————————————————————————————\n");
+	printf("* Bem-vindo a nosso jogo de adivinhação *");
+	printf("\n—————————————————————————————————————————");
+}
--- programa.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	printf("Bem vindo a nosso jogo de adivinhação");
-}
