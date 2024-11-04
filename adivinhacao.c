9d5139b6f0372d0f5eb99504a7229b3d0fca0178
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2024-11-03 21:28:17 -0300

    Criação de variáveis

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
 int main() {
+
+	//Variáveis
+	int numero_secreto;
+
 	//Imprime cabeçalho do jogo
 	printf("—————————————————————————————————————————\n");
 	printf("* Bem-vindo a nosso jogo de adivinhação *");
-	printf("\n—————————————————————————————————————————");
+	printf("\n—————————————————————————————————————————\n");
+
+	//Atribuição de valor
+	numero_secreto = 42;
+
+	printf("%d", numero_secreto);
 }
