7fba330b555bbd98fef8d6c40f994c5ef5b668b5
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2024-11-08 19:47:58 -0300

    Utilizando constante

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
+#define NUMERO_TENTATIVAS 5
+
 int main() {
 
 	//Variáveis
 	int numero_secreto;
 	int chute;
+	int numero_tentativas;
 
 	//Imprime cabeçalho do jogo
 	printf("******************************************\n");
@@ -13,9 +16,10 @@ int main() {
 
 	//Atribuição de valor
 	numero_secreto = 42;
+	numero_tentativas = 3;
 
-	for (int i = 1; i <= 3; i++) {
-		printf("\nTentativa %d de 3.", i);
+	for (int i = 1; i <= NUMERO_TENTATIVAS; i++) {
+		printf("\nTentativa %d de %d.", i, NUMERO_TENTATIVAS);
 		printf("\nDigite o seu chute: ");
 	
 		//Entrada de dados
@@ -25,7 +29,13 @@ int main() {
 
 		int acertou = chute == numero_secreto;
 		if (acertou) {
-			printf("Parabens! Voce acertou!");
+			printf("Parabens! Voce acertou!\n");
+			int mais_de_uma_tentativa = i > 1;
+			if (mais_de_uma_tentativa) {
+				printf("Foram %d tentativas.", i);
+			} else {
+				printf("Foi %d tentativa.", i);
+			}
 			break;
 		} else {
 			printf("Voce errou!\n");
