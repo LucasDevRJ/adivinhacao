92fd37a2bfb5d92363f8850a579ea856b0511ff6
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2024-11-15 17:58:31 -0300

    Nivel de dificuldade no jogo

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define CHANCES 5
+#define DECORACAO "******************************************"
 
 int main() {
 
@@ -13,36 +14,70 @@ int main() {
 	int acertos = 0;
 	int erros = 0;
 	char nome[50];
+	int nivel_dificuldade;
+	int valor_maximo;
+	int chances = 20;
+	int bonus_dificuldade;
 
 	//Imprime cabeçalho do jogo
-	printf("******************************************\n");
+	printf("%s\n", DECORACAO);
 	printf("* Bem-vindo a nosso jogo de adivinhacao *");
-	printf("\n******************************************\n");
+	printf("\n%s\n", DECORACAO);
 	printf("Digite o seu nome:");
 	scanf("%s", nome);
 
 	printf("\n");
-	printf("%s, voce tem %d tentativas\n", nome, CHANCES);
+	printf("1 - Nivel Facil: numero de 0 a 5 e 20 chances\n");
+	printf("2 - Nivel Normal: numero de 0 20 e 15 chances\n");
+	printf("3 - Nivel Dificil: numero de 0 a 40 e 10 chances\n");
+	printf("Digite o nivel desejado: ");
+	scanf("%d", &nivel_dificuldade);
 
-	while (tentativas < CHANCES) {
-		numero_secreto = rand() % 5;
-		if (numero_secreto == 0) {
-			numero_secreto = rand() % 5;
-			continue;
-		}
+	printf("\n");
 
-		printf("Tentativa numero %d de %d", tentativas + 1, CHANCES);
-		printf("\nDigite o seu chute: ");
+	int segundos = time(0);
+	srand(segundos);
 	
-		//Entrada de dados
+	while (tentativas < chances) {
+		printf("Digite o seu chute: ");	
 		scanf("%d", &chute);
 
-		if (chute <= 0 || chute > 5) {
-			printf("Digite um numero entre 1 e 5!\n");
+		int numero_randomico = rand();
+
+		if (nivel_dificuldade == 1) {
+			valor_maximo = 5;
+			bonus_dificuldade = 10.0;
+			numero_secreto = numero_randomico % valor_maximo;
+			if (chute < 0 || chute > 5) {
+				printf("Digite um numero entre 0 a 5!\n");
+				continue;
+			}
+		} else if (nivel_dificuldade == 2) {
+			chances = 15;
+			valor_maximo = 20;
+			bonus_dificuldade = 30.0;
+			numero_secreto = numero_randomico % valor_maximo;
+			if (chute < 0 || chute > 20) {
+				printf("Digite um numero entre 0 a 20!\n");
+				continue;
+			}
+		} else if (nivel_dificuldade == 3) {
+			chances = 10;
+			valor_maximo = 40;
+			bonus_dificuldade = 50.0;
+			numero_secreto = numero_randomico % valor_maximo;
+			if (chute < 0 || chute > 40) {
+				printf("Digite um numero entre 0 a 40!\n");
+				continue;
+			}
+		} else {
+			printf("Valor invalido! Digite um numero entre 1 a 3!\n");
 			continue;
 		}
 
-		printf("Seu chute e %d", chute);
+		//printf("%s, voce tem %d tentativas\n", nome, chances);
+		printf("%d\n", numero_secreto);
+		printf("\nTentativa numero %d de %d", tentativas + 1, chances);
 		tentativas++;
 		
 		int acertou = chute == numero_secreto;
@@ -50,8 +85,8 @@ int main() {
 
 		printf("\n\n");
 		if (acertou) {
-			printf("Parabens! Voce acertou!");
-			double calcula_pontos = (numero_secreto * 134.6)  / 1.7;
+			printf("Parabens! Voce acertou!\n");
+			double calcula_pontos = (numero_secreto * bonus_dificuldade * 83.4)  / 2.3;
 			pontos += calcula_pontos;
 			acertos++;
 		} else if (maior) {
